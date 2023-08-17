c9ff3a084d3e65e6e2410e0462c4cf3749d8f782
Queite <queitesc@gmail.com>
2023-08-17 17:58:14 -0300

    feat: :sparkles: implement error middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,17 @@
+import 'express-async-errors';
 import cors from 'cors';
 import express from 'express';
+import errorMiddleware from './middlewares/errorMiddleware';
 import UserController from './controller/UserController';
-import UserService from './model/UserService';
+import UserService from './service/UserService';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.post('/create', (req, res) => new UserController(req, res, new UserService()).createUser());
+app.get('/users', (req, res) => new UserController(req, res, new UserService()).getAll());
+
+app.use(errorMiddleware);
 
 export default app;
---
+++ backend/src/middlewares/errorMiddleware.ts
@@ -0,0 +1,25 @@
+import { Prisma } from '@prisma/client';
+import { NextFunction, Request, Response } from 'express';
+import HttpException from 'utils/httpException';
+import { ZodError } from 'zod';
+
+const errorMiddleware = (
+  err: Error | ZodError,
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+) => {
+  if (err instanceof ZodError) {
+    return res.status(400).json({ message: err.issues[0].message });
+  }
+  if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    return res.status(400).json({ message: 'Usuário já existente' });
+  }
+
+  const { status, message } = err as HttpException;
+  console.log("ERRRO", err);
+
+  res.status(status || 500).json({ message });
+};
+
+export default errorMiddleware;
