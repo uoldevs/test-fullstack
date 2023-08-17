24b97d470549f4ff83af70b472ca8ba7d1fa831c
Jonathan R. Andrade <jonathanreisdeandrade@gmail.com>
2023-08-17 16:46:05 -0300

    refactor: altera nome do modelo User para Client

---
+++ src/backend/prisma/migrations/20230817194514_init/migration.sql
@@ -0,0 +1,25 @@
+-- CreateTable
+CREATE TABLE "clients" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "cpf" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "statusId" INTEGER NOT NULL,
+    CONSTRAINT "clients_statusId_fkey" FOREIGN KEY ("statusId") REFERENCES "status" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "status" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "clients_email_key" ON "clients"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "clients_cpf_key" ON "clients"("cpf");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "status_name_key" ON "status"("name");
