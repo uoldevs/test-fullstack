419e159320e0bba5c880bdf8e98b8d00c81b3c68
pedro0505 <pedrohrds05@hotmail.com>
2023-08-17 14:21:32 -0300

    feat: adding prisma migrations

---
+++ backend/src/db/prisma/migrations/20230817170902_create_schema/migration.sql
@@ -0,0 +1,28 @@
+-- CreateTable
+CREATE TABLE "Status" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "client" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "cpf" TEXT NOT NULL,
+    "phone_number" TEXT NOT NULL,
+    "statusId" TEXT NOT NULL,
+    CONSTRAINT "client_statusId_fkey" FOREIGN KEY ("statusId") REFERENCES "Status" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Status_name_key" ON "Status"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "client_email_key" ON "client"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "client_cpf_key" ON "client"("cpf");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "client_phone_number_key" ON "client"("phone_number");
