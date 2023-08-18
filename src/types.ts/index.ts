fdc02cb6ce10172a7701e22b228b541b9d7f5bea
Filipe Braga <filipe.b.braga@gmail.com>
2023-08-18 14:59:07 -0300

    adding client type

---
+++ src/types.ts/index.ts
@@ -0,0 +1,11 @@
+export type ClientType = {
+    id: number;
+    name: string;
+    email: string;
+    cpf: string;
+    phone: string;
+    status: string;
+    createdAt: string;
+    updatedAt: string;
+    deletedAt: string;
+};
