5c03d9141822ff0109e7f4a47addc5d3113c8e6c
Rafael Nakashima <rafaelnakashima@rocketmail.com>
2023-08-18 19:49:53 -0300

    feat: added axios dependency and api file to conect backend with getUsers function

---
+++ frontend/src/services/api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const url = process.env.NEXT_PUBLIC_API_LINK || "http://localhost:3001/"
+
+export const api = axios.create({
+  baseURL: url,
+});
+
+export const getUsers = async () => {
+  try {
+    const { data } = await api.get('/users');
+    return data;
+  } catch (error) {
+    console.log(error)
+    const axiosError = error
+    return axiosError.response?.data;
+  }
+}
