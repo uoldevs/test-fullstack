35162581c6994cdc949457893c70b588ddf2cda2
pedro0505 <pedrohrds05@hotmail.com>
2023-08-18 15:20:52 -0300

    test: adding unit test to create in client service

--- backend/src/shared/entities/client/tests/service.spec.ts
+++ backend/src/shared/entities/client/tests/service.spec.ts
@@ -4,6 +4,8 @@ import ClientRepository from '../client.repository';
 import ClientService from '../client.service';
 import { Test, TestingModule } from '@nestjs/testing';
 import * as dataMock from './dataMock/clients';
+import { CreateClientDto } from '../dto/CreateClient.dto';
+import { ConflictException } from '@nestjs/common';
 
 describe('ClientService', () => {
   let clientService: ClientService;
@@ -33,6 +35,48 @@ describe('ClientService', () => {
       const result = await clientService.findAllClientsAndStatus();
 
       expect(result).toStrictEqual(dataMock.allClientsMock);
+      expect(clientRepository.findAllClientsAndStatus).toHaveBeenCalled();
+      expect(clientRepository.findAllClientsAndStatus).toHaveBeenCalledWith();
+    });
+  });
+
+  describe('create', () => {
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+
+    it('shoud return the info of the client that was created ', async () => {
+      jest
+        .spyOn(clientRepository, 'create')
+        .mockResolvedValue(dataMock.clientCreated);
+
+      jest.spyOn(clientService, 'checkConflicts').mockResolvedValue();
+
+      const client = new CreateClientDto(dataMock.clientToCreate);
+      const result = await clientService.create(client);
+
+      expect(result).toStrictEqual(dataMock.clientCreated);
+      expect(clientRepository.create).toHaveBeenCalled();
+      expect(clientRepository.create).toHaveBeenCalledWith(client);
+    });
+
+    it('shoud return the info of the client that was created ', async () => {
+      const errorMessage = 'Dados de úsuario que já estão cadastrado';
+
+      jest
+        .spyOn(clientRepository, 'create')
+        .mockResolvedValue(dataMock.clientCreated);
+
+      jest
+        .spyOn(clientService, 'checkConflicts')
+        .mockRejectedValue(new ConflictException(errorMessage));
+
+      const client = new CreateClientDto(dataMock.allClientsMock[0]);
+
+      await expect(clientService.create(client)).rejects.toThrowError(
+        errorMessage,
+      );
+      expect(clientRepository.create).not.toHaveBeenCalled();
     });
   });
 });
