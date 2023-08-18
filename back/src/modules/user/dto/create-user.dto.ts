d341b0ce441854b2d67594cf30c30130cc6b48f4
Filipe Braga <filipe.b.braga@gmail.com>
2023-08-18 16:20:06 -0300

    creating DTO to user

--- back/src/modules/user/dto/create-user.dto.ts
+++ back/src/modules/user/dto/create-user.dto.ts
@@ -1,1 +1,46 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsInt,
+  IsDate,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsOptional()
+  @IsInt()
+  id?: number;
+
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  phone: string;
+
+  @IsNotEmpty()
+  @IsString()
+  cpf: string;
+
+  @IsNotEmpty()
+  @IsString()
+  status: string;
+
+  @IsOptional()
+  @IsDate()
+  createdAt?: Date;
+
+  @IsOptional()
+  @IsDate()
+  updatedAt?: Date;
+
+  @IsOptional()
+  @IsDate()
+  deletedAt?: Date;
+}
