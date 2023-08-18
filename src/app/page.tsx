fe66058a0df63ad2f5f6adcdd3cd575a560434ba
Filipe Braga <filipe.b.braga@gmail.com>
2023-08-18 14:11:52 -0300

    adding class name to components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,37 @@
 import Image from 'next/image';
-import styles from './page.module.css';
+import './page.css';
 import { AppBar, Box, Button, Divider, Typography } from '@mui/material';
 import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 import '@fontsource/roboto/300.css';
 import uolLogo from '../../public/logo-uol.png';
+import ClientRow from '@/components/ClientRow';
 
 export default function Home() {
     return (
         <Box>
-            <AppBar
-                position="static"
-                sx={{ height: 60, display: 'flex', alignItems: 'center', backgroundColor: 'black' }}
-            >
+            <AppBar className="appBar">
                 <Image src={uolLogo} alt="Logo" height={50} />
             </AppBar>
-            <Box
-                sx={{
-                    width: '60%',
-                    margin: 'auto',
-                    marginTop: '70px',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    gap: '10px',
-                }}
-            >
-                <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'left' }}>
-                    <PersonOutlineOutlinedIcon sx={{ fontSize: 40 }} />
-                    <Typography sx={{ fontSize: 30 }}>Painel de clientes</Typography>
+            <Box className="mainContainer">
+                <Box className="clientPanelHeader">
+                    <PersonOutlineOutlinedIcon className="clientPanelIcon" />
+                    <Typography variant="h4">Painel de clientes</Typography>
                 </Box>
 
                 <Divider />
 
-                <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                <Box className="userListHeader">
                     <Box sx={{}}>
-                        <Typography sx={{ fontSize: 20 }}>Listagem de usuários</Typography>
-                        <Typography sx={{ fontSize: 20 }}>Escolha um cliente para visualizar os detalhes</Typography>
+                        <Typography variant="h5">Listagem de usuários</Typography>
+                        <Typography variant="subtitle1">Escolha um cliente para visualizar os detalhes</Typography>
                     </Box>
-                    <Button variant="contained" sx={{ fontSize: 10, backgroundColor: '#E29933' }}>
+                    <Button variant="contained" className="newClientButton">
                         Novo cliente
                     </Button>
+                </Box>
+
+                <Box>
+                    <ClientRow />
                 </Box>
             </Box>
         </Box>
