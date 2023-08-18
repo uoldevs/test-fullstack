1c0333c1f1044cd0821f730e5df83eff70186896
pedro0505 <pedrohrds05@hotmail.com>
2023-08-18 12:47:01 -0300

    refactor: adding a overload in create client dto

--- backend/src/shared/entities/client/dto/CreateClient.dto.ts
+++ backend/src/shared/entities/client/dto/CreateClient.dto.ts
@@ -7,4 +7,27 @@ export class CreateClientDto implements IClientStatus {
   public email: string;
   public phoneNumber: string;
   public status: IStatus;
+
+  constructor(data: CreateClientDto);
+  constructor(
+    name: string,
+    cpf: string,
+    email: string,
+    phoneNumber: string,
+    status: IStatus,
+  );
+
+  constructor(...args: any[]) {
+    if (args.length === 1 && typeof args[0] === 'object') {
+      Object.assign(this, args[0]);
+    } else if (args.length === 5) {
+      this.name = args[0];
+      this.cpf = args[1];
+      this.email = args[2];
+      this.phoneNumber = args[3];
+      this.status = args[4];
+    } else {
+      throw new Error('Invalid constructor arguments');
+    }
+  }
 }
