100dd468ad8aa3e55eadc33c0b2ee2597d122d4c
Filipe Braga <filipe.b.braga@gmail.com>
2023-08-18 15:40:06 -0300

    creating user entity

--- back/src/modules/user/entities/user.entity.ts
+++ back/src/modules/user/entities/user.entity.ts
@@ -1,1 +1,38 @@
-export class User {}
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  phone: string;
+
+  @Column()
+  cpf: string;
+
+  @Column()
+  status: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
