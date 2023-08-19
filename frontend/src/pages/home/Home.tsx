a0d98ad519ce7d3249c4182b7a7a767c83d42898
pedro0505 <pedrohrds05@hotmail.com>
2023-08-19 12:01:35 -0300

    feat: adding description in home page

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import PageHeader from '../../components/pageHeader/PageHeader';
 import ClientListingHeader from '../../components/clientListingHeader/ClientListingHeader';
+import './style.css';
 
 function Home() {
   return (
     <main>
       <PageHeader />
-      <ClientListingHeader />
+      <section className="home-page-container">
+        <div className="home-page-listing-container">
+          <ClientListingHeader className="home-page-listing-header" />
+          <div className="home-page-listing-view">
+            <header className="home-page-listing-view-header">
+              <h3>Listagem de usuários</h3>
+              <p>Escolha um cliente para vizualizar os detalhes</p>
+            </header>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
