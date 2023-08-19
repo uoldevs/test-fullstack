e7ffb1f17f1560b3e1fee04e76becdcc719da0dc
pedro0505 <pedrohrds05@hotmail.com>
2023-08-19 11:18:21 -0300

    feat: creating home page

---
+++ frontend/src/pages/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PageHeader from '../../components/pageHeader/PageHeader';
+import ClientListingHeader from '../../components/clientListingHeader/ClientListingHeader';
+
+function Home() {
+  return (
+    <main>
+      <PageHeader />
+      <ClientListingHeader />
+    </main>
+  );
+}
+
+export default Home;
