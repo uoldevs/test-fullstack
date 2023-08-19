f4a7f43c962708f54ff19ece137eb965aa97fa84
Filipe Braga <filipe.b.braga@gmail.com>
2023-08-19 09:08:10 -0300

    adicionando decorators

--- back/src/modules/user/dto/create-user.dto.ts
+++ back/src/modules/user/dto/create-user.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import {
   IsEmail,
   IsNotEmpty,
@@ -8,38 +9,66 @@ import {
 } from 'class-validator';
 
 export class CreateUserDto {
-  @IsOptional()
+  @ApiPropertyOptional({
+    description: 'ID do usuário',
+  })
+  @ApiProperty()
   @IsInt()
   id?: number;
 
+  @ApiProperty({
+    description: 'Nome do usuário',
+  })
   @IsNotEmpty()
   @IsString()
   name: string;
 
+  @ApiProperty({
+    description: 'Email do usuário',
+  })
   @IsNotEmpty()
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description: 'Telefone do usuário',
+  })
   @IsNotEmpty()
   @IsString()
   phone: string;
 
+  @ApiProperty({
+    description: 'CPF do usuário',
+  })
   @IsNotEmpty()
   @IsString()
   cpf: string;
 
+  @ApiProperty({
+    description: 'Status do usuário',
+    enum: ['Ativo', 'Desativado', 'Aguardando ativação', 'Inativo'],
+  })
   @IsNotEmpty()
   @IsString()
   status: string;
 
+  @ApiPropertyOptional({
+    description: 'Data de criação do usuário',
+  })
   @IsOptional()
   @IsDate()
   createdAt?: Date;
 
+  @ApiPropertyOptional({
+    description: 'Data de atualização do usuário',
+  })
   @IsOptional()
   @IsDate()
   updatedAt?: Date;
 
+  @ApiPropertyOptional({
+    description: 'Data de quando usuario foi deletado',
+  })
   @IsOptional()
   @IsDate()
   deletedAt?: Date;
