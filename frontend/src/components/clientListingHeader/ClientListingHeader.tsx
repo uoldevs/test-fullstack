7a9418bb216215717c6bb6c895aad240b39beccd
pedro0505 <pedrohrds05@hotmail.com>
2023-08-19 12:01:11 -0300

    feat: adding className props in ClientListingHeader

--- frontend/src/components/clientListingHeader/ClientListingHeader.tsx
+++ frontend/src/components/clientListingHeader/ClientListingHeader.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import { LuUser } from 'react-icons/lu';
 import './style.css';
 
-function ClientListingHeader() {
+interface ClientListingHeaderProps {
+  className?: string;
+}
+
+function ClientListingHeader({ className }: ClientListingHeaderProps) {
   return (
-    <div className="client-listing-header-container">
+    <div className={className ? `client-listing-header-container ${className}` : 'client-listing-header-container'}>
       <LuUser className="client-listing-header-user-icon" />
       <p className="client-listing-header-content">Painel de Clientes</p>
     </div>
