bc144cb1d6bc549b0da791b31991a9f2d2c38092
pedro0505 <pedrohrds05@hotmail.com>
2023-08-19 17:43:58 -0300

    feat: adding client edit page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
 import { ClientProvider } from './context/ClientContext';
+import EditClient from './pages/editClient/EditClient';
+import WebRoutes from './constants/WebRoutes';
 
 function App() {
   return (
     <>
       <ClientProvider>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path={WebRoutes.HOME} element={<Home />} />
+          <Route path={WebRoutes.CLIENT_EDIT} element={<EditClient />} />
         </Routes>
       </ClientProvider>
     </>
