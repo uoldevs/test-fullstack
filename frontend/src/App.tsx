ffe0cc35f344d7b39f6dbf8b32b891978440d29d
pedro0505 <pedrohrds05@hotmail.com>
2023-08-19 11:11:55 -0300

    feat: creating home page in '/'

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
 
 function App() {
-  return <h1>Hello World!!</h1>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
