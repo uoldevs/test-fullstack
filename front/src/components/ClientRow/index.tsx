d3546aedfb4ff699481c42f78ec6f9f802133f56
Filipe Braga <filipe.b.braga@gmail.com>
2023-08-18 23:52:43 -0300

    adding scss styles

---
+++ front/src/components/ClientRow/index.tsx
@@ -0,0 +1,48 @@
+import { Box, Button, Typography } from '@mui/material';
+import React from 'react';
+import styles from './style.module.scss';
+import { ClientType } from '@/types';
+import ColorDot from '../ColorDot/ColorDot';
+
+type ClientRowProps = {
+    client: ClientType;
+};
+
+const ClientRow: React.FC<ClientRowProps> = ({ client }) => {
+    return (
+        <Box className={styles.outterContainer}>
+            <Box className={styles.innerContainer}>
+                <Typography variant="h6" className={styles.innerText}>
+                    {client.name}
+                </Typography>
+                <Typography variant="subtitle1" className={styles.innerTextSubtitle1}>
+                    {client.email}
+                </Typography>
+            </Box>
+
+            <Box className={styles.innerContainer}>
+                <Typography variant="subtitle1" className={styles.innerText}>
+                    {client.cpf}
+                </Typography>
+                <Typography variant="subtitle1" className={styles.innerTextSubtitle1}>
+                    {client.phone}
+                </Typography>
+            </Box>
+
+            <Box className={styles.innerContainer2}>
+                <ColorDot status={client.status} />
+                <Typography variant="subtitle1" className={styles.innerText}>
+                    {client.status}
+                </Typography>
+            </Box>
+
+            <Box className={styles.innerContainer3}>
+                <Button variant="outlined" className={styles.editButton}>
+                    Editar
+                </Button>
+            </Box>
+        </Box>
+    );
+};
+
+export default ClientRow;
