4a43087162005836607afd903fe26cf85ed85d95
pedro0505 <pedrohrds05@hotmail.com>
2023-08-19 18:07:15 -0300

    feat: adding create client page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Home from './pages/home/Home';
 import { ClientProvider } from './context/ClientContext';
 import EditClient from './pages/editClient/EditClient';
 import WebRoutes from './constants/WebRoutes';
+import CreateClient from './pages/createClient/CreateClient';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path={WebRoutes.HOME} element={<Home />} />
           <Route path={WebRoutes.CLIENT_EDIT} element={<EditClient />} />
+          <Route path={WebRoutes.CREATE_CLIENT} element={<CreateClient />} />
         </Routes>
       </ClientProvider>
     </>
--- frontend/src/constants/WebRoutes.ts
+++ frontend/src/constants/WebRoutes.ts
@@ -1,6 +1,8 @@
 enum WebRoutes {
   HOME = '/',
   CLIENT_EDIT = '/client/edit',
+  CLIENT_EDIT_QUERRY_CLIENT_ID = '/client/edit?clientId=',
+  CREATE_CLIENT = '/client/create',
 }
 
 export default WebRoutes;
