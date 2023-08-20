05c579edf4eac907d90aff0eb1164bfd2c107df1
Filipe Braga <filipe.b.braga@gmail.com>
2023-08-20 00:35:20 -0300

    creating client form with validations

---
+++ front/src/components/ClientForm/Validation/index.tsx
@@ -0,0 +1,18 @@
+import * as yup from 'yup';
+
+export const schema = yup.object().shape({
+    name: yup.string().required('Nome é obrigatório'),
+    email: yup.string().email('Email inválido').required('Email é obrigatório'),
+    cpf: yup
+        .string()
+        .matches(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/, 'CPF inválido. Formato esperado: 999.999.999-99')
+        .required('CPF é obrigatório'),
+    phone: yup
+        .string()
+        .matches(
+            /^\(\d{2}\) \d? ?\d{4}-\d{4}$/,
+            'Telefone inválido. Formato esperado: (99) 9 9999-9999 ou (99) 9999-9999',
+        )
+        .required('Telefone é obrigatório'),
+    status: yup.string().required('Status é obrigatório'),
+});
