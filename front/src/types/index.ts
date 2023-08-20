475a68a9fe9f0337ec46688d893ebf11073f86b4
Filipe Braga <filipe.b.braga@gmail.com>
2023-08-20 00:37:25 -0300

    adding optional to id and dates to client type

--- front/src/types/index.ts
+++ front/src/types/index.ts
@@ -1,11 +1,11 @@
 export type ClientType = {
-    id: number;
+    id?: number;
     name: string;
     email: string;
     cpf: string;
     phone: string;
     status: string;
-    createdAt: string;
-    updatedAt: string;
-    deletedAt: string;
+    createdAt?: string;
+    updatedAt?: string;
+    deletedAt?: string;
 };
