525c77d125b1bfacd63097872d46c4756786fe6c
Filipe Braga <filipe.b.braga@gmail.com>
2023-08-20 00:34:07 -0300

    adding router push main page to clients

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,21 +1,15 @@
 'use client';
 import './globals.css';
-import styles from './style.module.scss';
-import { Box, Divider } from '@mui/material';
-import '@fontsource/roboto/300.css';
-import ClientTable from '@/components/ClientTable';
-import ClienHeader from '@/components/ClientHeader';
-import AppBarCustom from '@/components/AppBar';
+
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Home() {
-    return (
-        <Box>
-            <AppBarCustom />
-            <Box sx={{ marginTop: '125px' }} className={styles.mainContainer}>
-                <ClienHeader />
-                <Divider />
-                <ClientTable />
-            </Box>
-        </Box>
-    );
+    const router = useRouter();
+
+    useEffect(() => {
+        router.push('/clients');
+    });
+
+    return;
 }
