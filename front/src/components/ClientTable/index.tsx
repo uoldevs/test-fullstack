51ad872f29178cc77020f0fe7fa4964672a37175
Filipe Braga <filipe.b.braga@gmail.com>
2023-08-20 00:35:49 -0300

    removing mock and adding dispach for fetch users

--- front/src/components/ClientTable/index.tsx
+++ front/src/components/ClientTable/index.tsx
@@ -3,75 +3,20 @@
 import styles from './style.module.scss';
 import { Box, Typography, Button } from '@mui/material';
 import React, { useEffect, useState } from 'react';
-import apiHandler from '@/utils/apiHandler';
-import { ClientType } from '@/types';
-import ClientRow from '../ClientRow';
 import Loading from '../Loading';
-
-const clientsMock = [
-    {
-        id: 1,
-        name: 'John Doe',
-        email: 'john_doe@test.com',
-        cpf: '123.456.789-00',
-        phone: '(11)99999-9999',
-        status: 'Ativo',
-        createdAt: '',
-        updatedAt: '',
-        deletedAt: '',
-    },
-    {
-        id: 2,
-        name: 'John Doe',
-        email: 'john_doe@test.com',
-        cpf: '123.456.789-00',
-        phone: '(11)99999-9999',
-        status: 'Inativo',
-        createdAt: '',
-        updatedAt: '',
-        deletedAt: '',
-    },
-    {
-        id: 2,
-        name: 'John Doe',
-        email: 'john_doe@test.com',
-        cpf: '123.456.789-00',
-        phone: '(11)99999-9999',
-        status: 'Aguardando ativação',
-        createdAt: '',
-        updatedAt: '',
-        deletedAt: '',
-    },
-    {
-        id: 2,
-        name: 'John Doe',
-        email: 'john_doe@test.com',
-        cpf: '123.456.789-00',
-        phone: '(11)99999-9999',
-        status: 'Desativado',
-        createdAt: '',
-        updatedAt: '',
-        deletedAt: '',
-    },
-];
+import { useDispatch, useSelector } from 'react-redux';
+import ClientRow from '../ClientRow';
+import { fetchUsers } from '@/app/redux/slice/userSlice';
 
 const ClientTable = () => {
-    const [loading, setLoading] = useState(true);
-    const [clients, setClients] = useState([]);
+    const dispatch = useDispatch();
+    const users = useSelector((state) => state.users.entities);
+    const loading = useSelector((state) => state.users.loading);
 
     useEffect(() => {
-        fetchEvents();
-    }, []);
+        dispatch(fetchUsers());
+    }, [dispatch]);
 
-    const fetchEvents = async () => {
-        try {
-            const { data } = await apiHandler('GET', `user`);
-            setClients(data);
-            setLoading(false);
-        } catch (error) {
-            console.error('Error fetching events:', error);
-        }
-    };
     return (
         <Box>
             <Box className={styles.userListHeader}>
@@ -79,14 +24,19 @@ const ClientTable = () => {
                     <Typography variant="h5">Listagem de usuários</Typography>
                     <Typography variant="subtitle1">Escolha um cliente para visualizar os detalhes</Typography>
                 </Box>
-                <Button variant="contained" sx={{ backgroundColor: ' #c87a0d' }} className={styles.newClientButton}>
+                <Button
+                    href="/clients/new"
+                    variant="contained"
+                    sx={{ backgroundColor: ' #c87a0d' }}
+                    className={styles.newClientButton}
+                >
                     Novo cliente
                 </Button>
             </Box>
             {loading && <Loading />}
             {!loading && (
                 <Box>
-                    {clients.map((client: ClientType) => (
+                    {users.map((client: any) => (
                         <ClientRow key={client.id} client={client} />
                     ))}
                 </Box>
