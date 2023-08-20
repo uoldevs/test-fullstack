43cd003914527ac083c28c78da6643b5ce1ebf8a
Rafael <rafaelratier@hotmail.com>
2023-08-20 19:10:30 -0300

    feature: implements retrieving of all customers or specific one by id

--- backend/src/main/java/com/uol/backend/controller/CustomerController.java
+++ backend/src/main/java/com/uol/backend/controller/CustomerController.java
@@ -4,11 +4,10 @@ import com.uol.backend.model.Customer;
 import com.uol.backend.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/customer")
@@ -21,5 +20,17 @@ public class CustomerController {
     @ResponseStatus(HttpStatus.CREATED)
     public void createCustomer(@RequestBody Customer customer) {
         customerRepository.save(customer);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Customer> findAllCustomers() {
+        return customerRepository.findAll();
+    }
+
+    @GetMapping(value = "{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Optional<Customer> findCustomerById(@PathVariable Long id) {
+        return customerRepository.findById(id);
     }
 }
