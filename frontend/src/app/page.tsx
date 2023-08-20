0542616d2615e6a997c0220b6691ba2334c9f3b7
Jonathan R. Andrade <jonathanreisdeandrade@gmail.com>
2023-08-20 14:56:19 -0300

    chore(front): configura o nextjs

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Lista de Clientes",
+  description: "Lista de Clientes",
+};
+
+export default function Home() {
+  return <h1>Lista de Clientes</h1>;
+}
