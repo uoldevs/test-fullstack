6f03140991ecbda459f816f5142eaef0d083159e
Filipe Braga <filipe.b.braga@gmail.com>
2023-08-20 18:58:06 -0300

    router in the _app.tsx

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import { store } from '@/app/redux/store';
 import { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import { Provider } from 'react-redux';
 
 function MyApp({ Component, pageProps }: AppProps) {
+    const router = useRouter();
+    useEffect(() => {
+        router.push('/users');
+    });
+
     return (
         <Provider store={store}>
             <Component {...pageProps} />{' '}
