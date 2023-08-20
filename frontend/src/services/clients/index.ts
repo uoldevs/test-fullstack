f275c44c6182284a1c81a9384416fd53253db799
Jonathan R. Andrade <jonathanreisdeandrade@gmail.com>
2023-08-20 20:25:21 -0300

    refactor(front): altera tipo de retorno das funções em services

--- frontend/src/services/clients/index.ts
+++ frontend/src/services/clients/index.ts
@@ -1,3 +1,4 @@
+import { ApiError } from '../types';
 import { Client } from './types';
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
@@ -6,12 +7,12 @@ const headers = {
   'Content-Type': 'application/json'
 }
 
-export async function listClients(): Promise<Client[]>{
+export async function listClients(): Promise<Client[] | ApiError>{
   const response = await fetch(`${API_URL}/clients`);
   return response.json();
 }
 
-export async function createClient(client: Client): Promise<Client>{
+export async function createClient(client: Client): Promise<Client | ApiError>{
   const response = await fetch(`${API_URL}/clients`, {
     method: 'POST',
     headers,
@@ -20,7 +21,7 @@ export async function createClient(client: Client): Promise<Client>{
   return response.json();
 }
 
-export async function updateClient(id: number, client: Client): Promise<Client>{
+export async function updateClient(id: number, client: Client): Promise<Client | ApiError>{
   const response = await fetch(`${API_URL}/clients/${id}`, {
     method: 'PUT',
     headers,
--- frontend/src/services/status/index.ts
+++ frontend/src/services/status/index.ts
@@ -1,8 +1,9 @@
+import { ApiError } from '../types';
 import { Status } from './types';
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-export async function listStatus(): Promise<Status[]>{
+export async function listStatus(): Promise<Status[] | ApiError>{
   const response = await fetch(`${API_URL}/status`);
-  return response.json();
+  return response.json() as Promise<Status[] | ApiError>;
 }
