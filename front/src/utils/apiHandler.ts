8cdaed759482fe108d09facf289063b9ceb820bf
Filipe Braga <filipe.b.braga@gmail.com>
2023-08-20 17:00:10 -0300

    adding env logic

--- front/src/utils/apiHandler.ts
+++ front/src/utils/apiHandler.ts
@@ -3,9 +3,18 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function apiHandler(method: string, query: string, data?: object) {
     try {
+        let url: string;
+
+        const amplifyNodeEnv = await process.env.NEXT_PUBLIC_AMPLIFY_NODE_ENV;
+        if (amplifyNodeEnv === 'local') {
+            url = `http://localhost:3001/${query}`;
+        } else {
+            url = `https://kpvrxaf574.execute-api.us-east-1.amazonaws.com/dev/${query}`;
+        }
+
         const response: AxiosResponse = await axios({
             method: method as Method,
-            url: `http://localhost:3001/${query}`,
+            url,
             data,
         });
         return response;
