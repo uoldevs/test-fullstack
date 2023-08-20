631512a74eb8e1140c592a907598a0bd4d194339
Jonathan R. Andrade <jonathanreisdeandrade@gmail.com>
2023-08-20 13:19:00 -0300

    refactor(back): renomeia campo na tabela clients
    O arquivo de migração foi alterado porque o projeto não é compartilhado

--- backend/prisma/migrations/20230820161630_init/migration.sql
+++ backend/prisma/migrations/20230820161630_init/migration.sql
@@ -4,7 +4,7 @@ CREATE TABLE "clients" (
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "cpf" TEXT NOT NULL,
-    "phone" TEXT NOT NULL,
+    "phone_number" TEXT NOT NULL,
     "statusId" INTEGER NOT NULL,
     CONSTRAINT "clients_statusId_fkey" FOREIGN KEY ("statusId") REFERENCES "status" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
