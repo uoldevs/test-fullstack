546caef7ea0397813079a825d547e8ee5b8a3e5f
felipe_lima10@icloud.com <felipe_lima10@icloud.com>
2023-08-21 17:50:36 -0300

    test: list customer done

---
+++ web/__tests__/components/ListCustomers.spec.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import ListCustomers from '@/components/ListCustomers'
+import { renderWithrovider } from '@/lib/testing'
+import { returnCustomersSuccess } from '@/__tests__/store/sagas/customer.saga.spec'
+import page from '@/app/dashboard/page'
+
+describe('ListCustomers', () => {
+  it('renders all customers', () => {
+    const { getByTestId } = renderWithrovider(<ListCustomers />, {
+      customer: { ...returnCustomersSuccess.data },
+    })
+
+    returnCustomersSuccess.data.customers.forEach((customer) => {
+      expect(getByTestId(`card-customer-${customer.id}`)).toBeInTheDocument()
+    })
+  })
+
+  it('renders message "Nenhum cliente cadastrado!" when not have customers', () => {
+    const { getByTestId } = renderWithrovider(<ListCustomers />, {
+      customer: { customers: [], page: 1, quantity: 10, total: 2 },
+    })
+  })
+
+  it('render current page, next and prev button', () => {
+    const { getByTestId, getByText } = renderWithrovider(<ListCustomers />, {
+      customer: { ...returnCustomersSuccess.data },
+    })
+
+    expect(getByText(returnCustomersSuccess.data.page)).toBeInTheDocument()
+    expect(getByTestId('prev-button')).toBeInTheDocument()
+    expect(getByTestId('next-button')).toBeInTheDocument()
+  })
+
+  it('render text info customers list', () => {
+    const { getByText } = renderWithrovider(<ListCustomers />, {
+      customer: { ...returnCustomersSuccess.data },
+    })
+
+    const info = getByText(`Exibindo cliente de 1 a 2 de um total de 2`)
+    expect(info).toBeInTheDocument()
+  })
+})
