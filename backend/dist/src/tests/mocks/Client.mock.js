f77cca37d7e91dc6f3adb42329d79968964cd2c5
Gabriela Muniz <gabrielanamu@live.com>
2023-08-21 19:59:56 -0300

    chore: update Client.mock file

--- backend/dist/src/tests/mocks/Client.mock.js
+++ backend/dist/src/tests/mocks/Client.mock.js
@@ -5,24 +5,24 @@ const mock = [
         id: 1,
         name: 'test',
         email: 'test@live.com',
-        cpf: '07822832506',
-        phone: '(73) 99808-9918',
+        cpf: '88389016192',
+        phone: '(73) 99868-9718',
         status: 'Ativo'
     },
     {
         id: 2,
         name: 'test2',
         email: 'test2@live.com',
-        cpf: '07822832506',
-        phone: '(73) 99806-9918',
+        cpf: '69525293572',
+        phone: '(73) 99856-9412',
         status: 'Desativado'
     }
 ];
 const mockCreate = {
     name: 'test3',
     email: 'tes32@live.com',
-    cpf: '07822832506',
-    phone: '(73) 99806-9918',
+    cpf: '75504051150',
+    phone: '(73) 99820-9318',
     status: 'Desativado'
 };
 exports.default = {
