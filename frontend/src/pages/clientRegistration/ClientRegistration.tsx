aa89fda0a5770d1fe033d01035e3c3de92f739f9
Queite <queitesc@gmail.com>
2023-08-20 21:39:25 -0300

    refactor: :recycle: changes the information displayed by editing or adding client

--- frontend/src/pages/clientRegistration/ClientRegistration.tsx
+++ frontend/src/pages/clientRegistration/ClientRegistration.tsx
@@ -1,15 +1,23 @@
+import { useLocation } from "react-router-dom";
 import ClientForm from "../../components/clientForm/ClientForm";
 import HeaderClientDashbord from "../../components/headerClientDashbord/HeaderClientDashbord";
 import './clientRegistration.css'
 
 function ClientRegistration() {
+  const { pathname } = useLocation();
+  const create = pathname.includes('new');
+
   return(
     <div className="registration-container">
       <HeaderClientDashbord/>
       <div className="info-container">
         <div className="info">
-        <p className="info-title">Novo usuário</p>
-        <p className="info-p">Informe os campos a seguir para criar novo usuário</p>
+          <p className="info-title">Novo usuário</p>
+          {create
+            ? <p className="info-p">Informe os campos a seguir para cadastrar um novo cliente</p>
+            : <p className="info-p">Informe os campos a seguir para atualizar os dados do cliente</p>
+          }
+
         </div>
       </div>
       <ClientForm/>
