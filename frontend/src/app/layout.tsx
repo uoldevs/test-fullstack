f18e584bdf4cf82e1bf9629e039d5ab503e1b157
Jonathan R. Andrade <jonathanreisdeandrade@gmail.com>
2023-08-21 16:03:08 -0300

    feat(front): adiciona estilização no layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -16,14 +16,16 @@ export default function RootLayout({
     <html lang="pt">
       <body className={inter.className}>
         <AppProvider>
-          <header>
+          <header className="bg-black-900 p-3 flex justify-center">
             <Image src={logo} alt="UOL Logo" width={80} />
           </header>
-          <h1>
-            <User />
-            Painel de clientes
-          </h1>
-          {children}
+          <div className="max-w-5xl mx-auto">
+            <h1 className="flex items-center gap-4 text-black-950 text-2xl font-medium mt-16 pb-4 border-b border-black-200">
+              <User size={32} />
+              Painel de clientes
+            </h1>
+            {children}
+          </div>
         </AppProvider>
       </body>
     </html>
