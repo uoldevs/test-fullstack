b41dfe9e092352bc28245e9e290d6532f9b37661
Jonathan R. Andrade <jonathanreisdeandrade@gmail.com>
2023-08-21 14:27:16 -0300

    feat(front): adiciona layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,9 @@
-import "./globals.css";
+import { AppProvider } from "@/contexts";
 import { Inter } from "next/font/google";
+import { User } from "lucide-react";
+import "./globals.css";
+import logo from "../assets/uol-logo.png";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,7 +14,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AppProvider>
+          <header>
+            <Image src={logo} alt="UOL Logo" width={80} />
+          </header>
+          <h1>
+            <User />
+            Painel de clientes
+          </h1>
+          {children}
+        </AppProvider>
+      </body>
     </html>
   );
 }
