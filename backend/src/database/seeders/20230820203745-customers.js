7a3dde2c4eb13c4921dd7dbebb0968f61c356f31
Victor Hugo <victor.hiraiftw@outlook.com>
2023-08-21 10:52:44 -0300

    refactor: alterando cpf de seeders para cpf validos

--- backend/src/database/seeders/20230820203745-customers.js
+++ backend/src/database/seeders/20230820203745-customers.js
@@ -7,7 +7,7 @@ module.exports = {
         {
           name: 'John Doe',
           email: 'johndoe@email.com',
-          cpf: '256.366.978-16',
+          cpf: '350.887.750-75',
           phone: '(41) 99856-0101',
           status: 'Aguardando ativação',
         },
@@ -15,21 +15,21 @@ module.exports = {
         {
           name: 'Alan Turing',
           email: 'alanturing@email.com',
-          cpf: '123.456.789-10',
+          cpf: '496.013.860-25',
           phone: '(11) 94402-8922',
           status: 'Ativo',
         },
         {
           name: 'Ada Lovelace',
           email: 'adalovelace@email.com',
-          cpf: '256.366.978-17',
+          cpf: '727.669.940-41',
           phone: '(71) 92220-0305',
           status: 'Desativado',
         },
         {
           name: 'Nikola Tesla',
           email: 'tesla@email.com',
-          cpf: '987.654.321-17',
+          cpf: '257.891.020-00',
           phone: '(51) 92560-1025',
           status: 'Inativo',
         },
