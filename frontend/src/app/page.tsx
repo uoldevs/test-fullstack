b0228852509fb80eccf55b61e0bf290c5e6414fa
Jonathan R. Andrade <jonathanreisdeandrade@gmail.com>
2023-08-21 14:19:09 -0300

    feat(front): adiciona página inicial

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import Link from "next/link";
+import ClientList from "@/components/ClientList";
 
 export const metadata: Metadata = {
   title: "Lista de Clientes",
@@ -6,5 +8,16 @@ export const metadata: Metadata = {
 };
 
 export default function Home() {
-  return <h1>Lista de Clientes</h1>;
+  return (
+    <main>
+      <div>
+        <h2>Listagem de usuários</h2>
+        <p>Escolha um cliente para visualizar os detalhes</p>
+        <Link href="/clients/register">
+          <button type="button">Novo cliente</button>
+        </Link>
+      </div>
+      <ClientList />
+    </main>
+  );
 }
