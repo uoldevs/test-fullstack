7fbc3ac414576969b062070d7d18fadf771fcc45
Victor Hugo <victor.hiraiftw@outlook.com>
2023-08-21 10:41:33 -0300

    feat: page404, home e component header

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import uolLogo from '../images/uol-logo-full.svg';
+
+const Header = () => {
+  return (
+    <header>
+      <img src={uolLogo} alt="logo uol" />
+    </header>
+  );
+};
+
+export default Header;
