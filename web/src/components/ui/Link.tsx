0299b2c99832994a624b3743cb1c9db7e0f3b68b
felipe_lima10@icloud.com <felipe_lima10@icloud.com>
2023-08-21 17:29:42 -0300

    feat: add link ui

---
+++ web/src/components/ui/Link.tsx
@@ -0,0 +1,23 @@
+import { cn } from '@/lib/utils'
+import { AnchorHTMLAttributes } from 'react'
+import { buttonVariants } from '@/styles/variants/button'
+import { VariantProps } from 'class-variance-authority'
+import LinkNext from 'next/link'
+
+interface LinkProps
+  extends AnchorHTMLAttributes<HTMLAnchorElement>,
+    VariantProps<typeof buttonVariants> {
+  href: string
+}
+
+function Link({ children, variant, size, className, ...rest }: LinkProps) {
+  const linkClassName = cn(buttonVariants({ variant, size, className }))
+
+  return (
+    <LinkNext className={linkClassName} {...rest}>
+      {children}
+    </LinkNext>
+  )
+}
+
+export default Link
