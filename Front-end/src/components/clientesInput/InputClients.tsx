dd482a9716d0c03c01bb7eb7e06fa382c235837b
Lucas Dias Alencar <lucaalencarid@gmail.com>
2023-08-21 20:48:10 -0300

    ajustes na estrutura do front-end

--- Front-end/src/components/clientesInput/InputClients.tsx
+++ Front-end/src/components/clientesInput/InputClients.tsx
@@ -1,72 +1,101 @@
-import React, { useState } from 'react';
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import Api from '../../services/resquest';
-import { AxiosResponse } from 'axios';
+type propTypeInput = {
+  name: string;
+  sobrenome: string;
+  email: string;
+  cpf: string;
+  phone: string;
+  status: string;
+  setName: (value: string) => void;
+  setSobrenome: (value: string) => void;
+  setEmail: (value: string) => void;
+  setCpf: (value: string) => void;
+  setPhone: (value: string) => void;
+  setStatus: (value: string) => void;
+  updateHandle: () => void;
+};
 
-const InputClients = () => {
-  const location = useLocation();
-  console.log(location)
-  const [name, setName] = useState(location.state.firstName);
-  const [Sobrenome, setSobreNome]= useState(location.state.lastName);
-  const [email, setEmail] = useState(location.state.email);
-  const [cpf, setCpf] = useState(location.state.cpf);
-  const [phone, setPhone] = useState(location.state.cellphone);
-  const [status, setStatus] = useState(location.state.status);
-  const { id } = useParams();
+const InputClients = (props: propTypeInput) => {
   const navigate = useNavigate();
-
-  const updateHandle = async () => {
-
-    const updatedClient = {
-        id: Number(id),
-        firstName: name,
-        lastName: Sobrenome,
-        email,
-        cpf,
-        cellphone: phone,
-        status: status.toLowerCase(),
-    }
-    const response: AxiosResponse = await Api.putUpdate('/', updatedClient);
-    if (response.status === 200) {
-        navigate('/');
-    }
-  }
-
+  const {
+    name,
+    setName,
+    sobrenome,
+    setSobrenome,
+    email,
+    setEmail,
+    cpf,
+    setCpf,
+    phone,
+    setPhone,
+    status,
+    setStatus,
+    updateHandle,
+  } = props;
   return (
     <div>
       <div>
         <label>Nome:</label>
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        <input
+          type='text'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
       </div>
       <div>
         <label>Sobrenome:</label>
-        <input type="text" value={Sobrenome} onChange={(e) => setSobreNome(e.target.value)} />
+        <input
+          type='text'
+          value={sobrenome}
+          onChange={(e) => setSobrenome(e.target.value)}
+        />
       </div>
       <div>
         <label>Email:</label>
-        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input
+          type='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
       <div>
         <label>CPF:</label>
-        <input type="text" value={cpf} onChange={(e) => setCpf(e.target.value)} />
+        <input
+          type='text'
+          value={cpf}
+          onChange={(e) => setCpf(e.target.value)}
+        />
       </div>
       <div>
         <label>Phone:</label>
-        <input type="text" value={phone} onChange={(e) => setPhone(e.target.value)} />
+        <input
+          type='text'
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+        />
       </div>
       <div>
         <label>Status:</label>
-        <select value={status} onChange={(e) => {setStatus(e.target.value)}}>
-          <option value="ativo">Ativo</option>
-          <option value="inativo">Inativo</option>
-          <option value="desativado">Desativado</option>
-          <option value="ativação_pendente">Aguardando ativaçao</option>
+        <select
+          value={status}
+          onChange={(e) => {
+            setStatus(e.target.value);
+          }}>
+          <option value='ativo'>Ativo</option>
+          <option value='inativo'>Inativo</option>
+          <option value='desativado'>Desativado</option>
+          <option value='ativação_pendente'>Aguardando ativaçao</option>
         </select>
       </div>
       <div>
-        <button type='button' onClick={updateHandle}>Salvar</button>
-        <button type='button' onClick={() => navigate('/')}>Cancelar</button>
+        <button type='button' onClick={updateHandle}>
+          Salvar
+        </button>
+        <button type='button' onClick={() => navigate('/')}>
+          Cancelar
+        </button>
       </div>
     </div>
   );
--- Front-end/src/pages/clientsEditPage/EditClient.tsx
+++ Front-end/src/pages/clientsEditPage/EditClient.tsx
@@ -1,17 +1,58 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { AxiosResponse } from 'axios';
+
+import Api from '../../services/resquest';
 import InputClients from '../../components/clientesInput/InputClients';
 import './EditClient.css'
 
-class EditClient extends Component {
+const EditClient = () => {
+    const location = useLocation();
+    const [name, setName] = useState(location.state.firstName);
+    const [sobrenome, setSobrenome]= useState(location.state.lastName);
+    const [email, setEmail] = useState(location.state.email);
+    const [cpf, setCpf] = useState(location.state.cpf);
+    const [phone, setPhone] = useState(location.state.cellphone);
+    const [status, setStatus] = useState(location.state.status);
+    const { id } = useParams();
+    const navigate = useNavigate();
 
-  render() {
+    const updateHandle = async () => {
+
+        const updatedClient = {
+            id: Number(id),
+            firstName: name,
+            lastName: sobrenome,
+            email,
+            cpf,
+            cellphone: phone,
+            status: status.toLowerCase(),
+        }
+        const response: AxiosResponse = await Api.putUpdate('/', updatedClient);
+        if (response.status === 200) {
+            navigate('/');
+        }
+      }
     return (
       <div className='clients-view-main'>
         <h2>Painel de clientes</h2>
-        <InputClients/>
+        <InputClients
+         name={name}
+         sobrenome={sobrenome}
+         email={email}
+         cpf={cpf}
+         phone={phone}
+         status={status}
+         setName={setName}
+         setSobrenome={setSobrenome}
+         setEmail={setEmail}
+         setCpf={setCpf}
+         setPhone={setPhone}
+         setStatus={setStatus}
+         updateHandle={updateHandle}
+        />
       </div>
     )
-  }
 }
 
 export default EditClient;
