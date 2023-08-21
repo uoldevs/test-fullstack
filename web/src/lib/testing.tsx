498cfe08b00c52b6834b7c3cfa0bbdcb821376b5
felipe_lima10@icloud.com <felipe_lima10@icloud.com>
2023-08-21 17:30:50 -0300

    feat: add render with provider

---
+++ web/src/lib/testing.tsx
@@ -0,0 +1,18 @@
+import React, { ReactNode } from 'react'
+import { Provider } from 'react-redux'
+import { render } from '@testing-library/react'
+import { configureStore } from '@reduxjs/toolkit'
+import { reducers } from '@/store/index'
+
+const wraperWithProvider = (children: ReactNode, mockedStore: object) => {
+  const store = configureStore({
+    reducer: reducers,
+    preloadedState: mockedStore,
+  })
+
+  return <Provider store={store}>{children}</Provider>
+}
+
+export const renderWithrovider = (component: ReactNode, mockedStore = {}) => {
+  return render(wraperWithProvider(component, mockedStore))
+}
