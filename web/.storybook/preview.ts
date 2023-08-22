5f49a310996c916673555c576bd37a87f285dce0
felipe_lima10@icloud.com <felipe_lima10@icloud.com>
2023-08-21 22:56:43 -0300

    feat: install storybook

---
+++ web/.storybook/preview.ts
@@ -0,0 +1,18 @@
+import '../src/styles/globals.css'
+import type { Preview } from "@storybook/react";
+
+import { themes } from '@storybook/theming'
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    }
+  },
+};
+
+export default preview;
