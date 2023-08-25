d3dd36fe69dd28889b93b737271ffd4c3eff6517
Jonathan R. Andrade <jonathanreisdeandrade@gmail.com>
2023-08-25 01:52:20 -0300

    refactor(front): altera renderização do texto de ajuda do select

--- frontend/src/components/Form/components/Select.tsx
+++ frontend/src/components/Form/components/Select.tsx
@@ -48,9 +48,11 @@ const Select = (
           {label}
         </label>
       </div>
-      <p className="mt-2 mb-4 ml-1 text-sm text-valencia-600" role="alert">
-        {helperText}
-      </p>
+      {helperText && (
+        <p className="mt-2 mb-4 ml-1 text-sm text-valencia-600" role="alert">
+          {helperText}
+        </p>
+      )}
     </div>
   );
 };
